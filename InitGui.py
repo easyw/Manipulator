b3fecc4065563d6b813bcb70a7e97cb8ca59a7ac
easyw <easyw@users.noreply.github.com>
2018-09-11 14:33:57 +0200

    aligning commit nbrs

--- InitGui.py
+++ InitGui.py
@@ -24,7 +24,7 @@ MWB_wb_version='v 1.2.6'
 global myurlMWB
 myurlMWB='https://github.com/easyw/Manipulator'
 global mycommitsMWB
-mycommitsMWB=110 #v 1.2.6
+mycommitsMWB=112 #v 1.2.6
 
 
 import FreeCAD, FreeCADGui, Part, os, sys
