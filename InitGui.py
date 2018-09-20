c8d360871af661ef254bb7bd92d42b7129cdbfbd
easyw <easyw@katamail.com>
2018-09-20 11:24:05 +0200

    better Aliogner ico

--- InitGui.py
+++ InitGui.py
@@ -24,7 +24,7 @@ MWB_wb_version='v 1.2.6'
 global myurlMWB
 myurlMWB='https://github.com/easyw/Manipulator'
 global mycommitsMWB
-mycommitsMWB=113 #v 1.2.6
+mycommitsMWB=114 #v 1.2.6
 
 
 import FreeCAD, FreeCADGui, Part, os, sys
